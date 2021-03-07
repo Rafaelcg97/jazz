80240de1d7565e1e5ed7514d2697180b56e0c702
Rafael Castro <castroguzmanrafa@outlook.com>
2021-03-07 11:20:49 -0600

    2021-03-07

--- Production_control 1.0/impresion.xaml.cs
+++ Production_control 1.0/impresion.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Printing;
 using System.Drawing.Printing;
+using NHibernate.Impl;
 
 namespace Production_control_1._0
 {
@@ -22,45 +23,119 @@ namespace Production_control_1._0
     /// </summary>
     public partial class impresion : Page
     {
+        #region clases_especiales
         private PrintDocument printDoc = new PrintDocument();
+        #endregion
+
+        #region datos_iniciales
         public impresion()
         {
             InitializeComponent();
 
-            string pkInstalledPrinters;
+            //agregar la lista de impresoras instaladas
+            string impresora_instalada;
             for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
             {
-                pkInstalledPrinters = PrinterSettings.InstalledPrinters[i];
-                impresora.Items.Add(pkInstalledPrinters);
+                impresora_instalada = PrinterSettings.InstalledPrinters[i];
+                impresora.Items.Add(impresora_instalada);
+                if (printDoc.PrinterSettings.IsDefaultPrinter)
+                {
+                    impresora.SelectedItem = printDoc.PrinterSettings.PrinterName;
+                }
             }
 
+            //agregar orientaciones de paginas
+            orientacion_impresion.Items.Add("Horizontal");
+            orientacion_impresion.Items.Add("Vertical");
 
+            //agregar tamaños de paginas
+            tamano_impresion.Items.Add("Tabloide");
+            tamano_impresion.Items.Add("Carta");
+            tamano_impresion.Items.Add("Oficio");
+
+            //establecer orientacion predeterminada
+            orientacion_impresion.SelectedItem = "Horizontal";
+
+            //establecer tamaño predeterminado
+            tamano_impresion.SelectedItem="Tabloide";
+
+            //numero de copias
+            copias.Text = "1";
 
 
         }
 
+        #endregion
+
+        #region calculos_generales
+        private void solo_numeros(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D1 && e.Key <= Key.D9 || e.Key >= Key.NumPad1 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+        #endregion
+
+        #region formulario_impresion
         private void confirmar_impresion_Click(object sender, RoutedEventArgs e)
         {
-            
-            PageMediaSize pageMediaSize = new PageMediaSize(PageMediaSizeName.NorthAmericaTabloid);
+            //varibales para hacer el switch de los tamaños de pagina y orientaciones de pagina
+            string tamano_pagina=tamano_impresion.SelectedItem.ToString();
+            string orientacion_pagina = tamano_impresion.SelectedItem.ToString();
+            PageMediaSize tamano;
+            PageOrientation orientacion;
+
+
+            //configurar el valor del tamño de pagina
+            switch (tamano_pagina)
+                 {
+                case "Carta":
+                    tamano = new PageMediaSize(PageMediaSizeName.NorthAmericaLetter);
+                    break;
+                case "Oficio":
+                    tamano = new PageMediaSize(PageMediaSizeName.NorthAmericaLegal);
+                    break;
+                case "Tabloide":
+                    tamano = new PageMediaSize(PageMediaSizeName.NorthAmericaTabloid);
+                    break;
+                default:
+                    tamano = new PageMediaSize(PageMediaSizeName.NorthAmericaTabloid);
+                    break;
+            };
+
+            //configurar el valor de orientacion de pagina
+            switch (orientacion_pagina)
+            {
+                case "Horizontal":
+                   orientacion = PageOrientation.Landscape;
+                    break;
+                case "Vertical":
+                    orientacion = PageOrientation.Portrait;
+                    break;
+                default:
+                    orientacion= PageOrientation.Landscape;
+                    break;
+            };
+
+            //establecer las propiedades de impresion
             PrintDialog dialog = new PrintDialog();
-            dialog.PrintTicket.PageOrientation = PageOrientation.Landscape;
+            dialog.PrintTicket.PageOrientation = orientacion;
             dialog.PrintTicket.PageBorderless = PageBorderless.None;
-            dialog.PrintTicket.PageMediaSize = pageMediaSize;
-            printDoc.DefaultPageSettings.PrinterSettings.PrinterName = impresora.SelectedItem.ToString();
+            dialog.PrintTicket.PageMediaSize = tamano;
+            dialog.PrintTicket.CopyCount = Convert.ToInt32(copias.Text);
+            PrinterSettings ps = new PrinterSettings();
+
+            PrintDialog printDialog = new PrintDialog();
+            dialog.PrintQueue = new PrintQueue(new PrintServer(), impresora.SelectedItem.ToString());
             dialog.PrintVisual(area_de_impresion, "LayOut");
-
-            // Imprimir la pantalla
-
-
-            //if (dialog.ShowDialog() == true)
-            //{
-            //dialog.PrintVisual(this, "Impresión");
-            //}
-
-
         }
 
+        private void aumentar_copias_Click(object sender, RoutedEventArgs e)
+        {
+            copias.Text = (Convert.ToInt32(copias.Text) + 1).ToString();
+        }
 
+        #endregion
     }
 }
