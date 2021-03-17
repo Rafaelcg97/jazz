0454862b0951359e225e30b4fcb2a448c411de54
Rafael Castro <rafael.castro@texops.com>
2021-03-17 15:51:20 -0600

    2021-03-17

---
+++ Production_control 1.0/clases globales/usuario_respuesto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Production_control_1._0.clases_globales
+{
+    class usuario_respuesto
+    {
+         public static int usuario { get; set; }
+    }
+}
--- Production_control 1.0/sesion_mecanico.xaml.cs
+++ Production_control 1.0/sesion_mecanico.xaml.cs
@@ -20,11 +20,16 @@ namespace Production_control_1._0
 {
     public partial class sesion_mecanico : Page
     {
+        #region variables_generales
         public SqlConnection cn = new SqlConnection("Data Source=" + ConfigurationManager.AppSettings["servidor_ing"] + ";Initial Catalog=" + ConfigurationManager.AppSettings["base_manto"] + ";Persist Security Info=True;User ID=" + ConfigurationManager.AppSettings["usuario_ing"] + ";Password=" + ConfigurationManager.AppSettings["pass_ing"]);
+        #endregion
+
+        #region datos_iniciales
         public sesion_mecanico()
         {
             InitializeComponent();
         }
+        #endregion
 
         #region control_general_del_programa
 
@@ -61,5 +66,53 @@ namespace Production_control_1._0
 
         #endregion
 
+        #region formulario_ingreso
+
+        private void Ingresar_Click(object sender, RoutedEventArgs e)
+        {
+            //obtener la contrasena ingresada
+            try
+            {
+                int contrasena_introducida = Convert.ToInt32(password.Password);
+                int usuario_introducido = Convert.ToInt32(Usuario.Text);
+                  
+            //se realiza la consulta en la base de cual es la contrasena del usuario
+            string sql = "select contrasena from mecanicos where codigo=" + usuario_introducido;
+            cn.Open();
+            SqlCommand cm = new SqlCommand(sql, cn);
+            cm.ExecuteNonQuery();
+            SqlDataReader dr = cm.ExecuteReader();
+            dr.Read();
+            try
+            {
+                int contrasena_correcta = Convert.ToInt32(dr["contrasena"]);
+                dr.Close();
+                cn.Close();
+                //se valida si la contrasena del usuario es la misma que la que se introduji
+                if (contrasena_introducida == contrasena_correcta)
+                {
+                    clases_globales.usuario_respuesto.usuario = usuario_introducido;
+                    solicitud_repuestos solicitud_repuestos = new solicitud_repuestos();
+                    this.NavigationService.Navigate(solicitud_repuestos);
+                }
+                else
+                {
+                    MessageBox.Show("Contraseña Incorrecta");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Usuario Incorrecto");
+                cn.Close();
+            }
+            }
+            catch
+            {
+                MessageBox.Show("Hay un campo que no se ha llenado");
+            }
+
+        }
+
+        #endregion
     }
 }
