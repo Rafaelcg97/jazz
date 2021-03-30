95169f2d1c1e19c8b1c96493f91fe7bd67b2a270
Rafael Castro <rafael.castro@texops.com>
2021-03-30 16:14:15 -0600

    2021-03-30

--- Production_control 1.0/clases/maquina.cs
+++ Production_control 1.0/clases/maquina.cs
@@ -10,5 +10,11 @@ namespace Production_control_1._0.clases
     {
         public string ajusteMaquina { get; set; }
         public string categoriaMaquina { get; set; }
+        public string colorAjuste { get; set; }
+        public int ii { get; set; }
+        public int ie { get; set; }
+        public int ee { get; set; }
+        public int ei { get; set; }
+        public int na { get; set; }
     }
 }
